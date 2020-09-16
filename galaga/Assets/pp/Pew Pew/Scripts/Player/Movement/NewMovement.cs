58e84c5742f53ce54aff5c3c03492483b8570f94
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2020-09-16 14:54:10 -0400

    movement

---
+++ galaga/Assets/pp/Pew Pew/Scripts/Player/Movement/NewMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewMovement : MonoBehaviour
+{
+    public Slider slider;
+    private float sliderValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderValue = slider.value;
+        Vector3 temp = transform.position;
+        temp.x = sliderValue;
+        transform.localPosition = temp;
+    }
+}
